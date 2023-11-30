98eed331e85daeb931af99850178568a322b6b3f
Danil Alekseenko <maddantop@gmail.com>
2023-11-30 11:47:18 +1000

    Commit made with deepseek aboba

--- src/01_Calculator/calculator.py
+++ src/01_Calculator/calculator.py
@@ -1,43 +1,56 @@
 from operator import add, mul, sub, truediv
 from typing import List, Optional, Union
 
-ops = {"+": add, "-": sub, "*": mul, "/": truediv}
+
+def prefix_evaluate(prefix_evaluation: List[str]) -> int:
+    stack = []
+    operators = set(['+', '-', '*', '/'])
+
+    for i in reversed(range(len(prefix_evaluation))):
+        if prefix_evaluation[i] in operators:
+            a = stack.pop()
+            b = stack.pop()
+            if prefix_evaluation[i] == '+':
+                stack.append(a + b)
+            elif prefix_evaluation[i] == '-':
+                stack.append(a - b)
+            elif prefix_evaluation[i] == '*':
+                stack.append(a * b)
+            elif prefix_evaluation[i] == '/':
+                stack.append(int(a / b))
+        elif prefix_evaluation[i].strip():  # Skip spaces
+            stack.append(int(prefix_evaluation[i]))
+
+    return stack[0]
 
 
-def _split_if_string(string_or_list: Union[List[str], str]) -> List[str]:
-    return string_or_list.split() if isinstance(string_or_list, str) else string_or_list
+def to_prefix(equation: str) -> List[str]:
+    stack = []
+    output = []
+    operators = set(['+', '-', '*', '/', '(', ')'])
+    precedence = {'+':1, '-':1, '*':2, '/':2}
 
+    equation = equation.replace(" ", "")
+    equation = list(equation)[::-1]
 
-def prefix_evaluate(prefix_equation: Union[List[str], str]) -> Optional[int]:
-    if not prefix_equation:
-        return None
-    prefix_equation = _split_if_string(prefix_equation)
-    value_stack = []
-    while prefix_equation:
-        el = prefix_equation.pop()
-        if el not in ops:
-            value_stack.append(int(el))
+    for char in equation:
+        if char not in operators:
+            output.append(char)
+        elif char==')':
+            stack.append(')')
+        elif char=='(':
+            while stack and stack[-1]!=')':
+                output.append(stack.pop())
+            stack.pop()
         else:
-            r_val = value_stack.pop()
-            l_val = value_stack.pop()
-            operation = ops[el]
-            value_stack.append(operation(r_val, l_val))
+            while stack and stack[-1]!=')' and precedence[char]<=precedence.get(stack[-1], 0):
+                output.append(stack.pop())
+            stack.append(char)
 
-    return value_stack[0]
+    while stack:
+        output.append(stack.pop())
 
-
-def to_prefix(equation: str) -> str:
-    op_stack = []
-    prefix = []
-
-    for el in equation.split()[::-1]:
-        pass
-
-    while op_stack:
-        prefix.append(op_stack.pop())
-
-    return " ".join(prefix[::-1])
-
+    return output[::-1]
 
 def calculate(equation: str) -> int:
     return prefix_evaluate(to_prefix(equation))
